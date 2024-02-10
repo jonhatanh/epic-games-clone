4bb87426cb8a7f9ad2fb3d9c104c9a262078ec63
jonhatanh <jonhatan.higuera@gmail.com>
2024-02-10 09:36:06 -0600

    Update Nav and Store nav

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,10 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import exampleImg from '../../assets/exampleImage.avif'
+import classes from './Store.module.css'
 
 const Store = () => {
   return (
     <div>
-      <p>STORE PAGE</p>
-      <Link to='/'> Go to Welcome Page </Link>
+      {/* Nav store */}
+      <nav className={classes.nav}>
+        <input
+          type='search'
+          name='search'
+          id='search'
+
+          placeholder='Search store'
+          className={classes.searchBar}
+        />
+        <Link className={classes.link} to='discover'>
+          Discover
+        </Link>
+        <Link className={classes.link} to='browse'>
+          Browse
+        </Link>
+      </nav>
+      <section>{/* <img src={exampleImg} alt="" /> */}</section>
     </div>
   )
 }
