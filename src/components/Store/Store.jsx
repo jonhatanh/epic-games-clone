8e7021246f6204856a1a74abe79a8a4b637110a4
jonhatanh <jonhatan.higuera@gmail.com>
2024-02-23 19:35:43 -0600

    WIP sstore styles

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,7 +1,7 @@
-import { Link, useLoaderData } from 'react-router-dom';
-import exampleImg from '../../assets/exampleImage.avif';
-import classes from './Store.module.css';
-import { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom'
+import exampleImg from '../../assets/exampleImage.avif'
+import classes from './Store.module.css'
+import { useState } from 'react'
 
 function returnCopies (game, qty = 1) {
   const copies = []
@@ -17,54 +17,71 @@ const Store = () => {
   const game2 = heroGames.results[1]
   console.log(heroGames)
   return (
-    <div className="">
+    <div className=''>
       {/* Nav store */}
       <nav className={classes.nav}>
         <input
-          type="search"
-          name="search"
-          id="search"
-          placeholder="Search store"
+          type='search'
+          name='search'
+          id='search'
+          placeholder='Search store'
           className={classes.searchBar}
         />
-        <Link className={classes.link} to="discover">
+        <Link className={classes.link} to='discover'>
           Discover
         </Link>
-        <Link className={classes.link} to="browse">
+        <Link className={classes.link} to='browse'>
           Browse
         </Link>
       </nav>
-      <section className="">
-        {/* last 30 days */}
+      <section className={classes.storeMain}>
+        {/* best last six months */}
         <article className={classes.heroGames}>
-          {returnCopies(game1, 7).map((game) => {
-            return <div className={classes.cardHero}>{game.name}</div>;
+          <div className={classes.mainGame}>
+            <img
+              src={game1.background_image}
+              alt={`${game1.name} background image`}
+            />
+            <h3>{game1.name}</h3>
+            <button>Buy Now</button>
+            <button>Add to wishlist</button>
+          </div>
+          {returnCopies(game1, 6).map((game, index) => {
+            return (
+              <div key={index} className={classes.cardHero}>
+                <img
+                  src={game.background_image}
+                  alt={`${game.name} background image`}
+                />
+                <h3>{game.name}</h3>
+              </div>
+            )
           })}
         </article>
 
         {/* bY META CRITIC  this year */}
         <article className={classes.listOfGamesHorizontal}>
           {returnCopies(game2, 6).map((game) => {
-            return <div className={classes.cardHero}>{game.name}</div>;
+            return <div className={classes.cardHero}>{game.name}</div>
           })}
         </article>
 
-        {/* random games */}
+        {/* random games and last 30 days */}
         <article className={classes.listOfGamesVertical}>
           {returnCopies(game1, 15).map((game) => {
-            return <div className={classes.cardHero}>{game.name}</div>;
+            return <div className={classes.cardHero}>{game.name}</div>
           })}
         </article>
 
         {/* all time best */}
         <article className={classes.listOfGamesHorizontal}>
           {returnCopies(game2, 6).map((game) => {
-            return <div className={classes.cardHero}>{game.name}</div>;
+            return <div className={classes.cardHero}>{game.name}</div>
           })}
         </article>
       </section>
     </div>
-  );
-};
+  )
+}
 
 export default Store
