adebe989e47c392400e03e6f2985c654348ecd77
jonhatanh <jonhatan.higuera@gmail.com>
2024-03-06 14:07:24 -0600

    Change GameOverview layout

--- src/components/GameOverview/GameOverview.jsx
+++ src/components/GameOverview/GameOverview.jsx
@@ -7,11 +7,13 @@ import {
   faCaretRight,
   faChevronLeft,
   faChevronRight,
-  faCirclePlus
+  faCirclePlus,
 } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
 import Button from '../Button/Button'
 import React from 'react'
 import Collapsable from '../Collapsable/Collapsable'
+import Rating from '../Rating/Rating'
 
 function justEnglishDescription (description) {
   description = description.replaceAll('<br />', '<br /><br />')
@@ -22,7 +24,7 @@ const GameOverview = () => {
   const { game, achievements } = useLoaderData()
   console.log(game)
   return (
-    <div className={classes.container}>
+    <article className={classes.container}>
       <h1 className={classes.title}>{game.name}</h1>
       <nav>
         <ul>
@@ -30,13 +32,13 @@ const GameOverview = () => {
           <li>Logros</li>
         </ul>
       </nav>
-      <div className={classes.gameDetails}>
+      <section className={classes.gameDetails}>
         {/* Game Info */}
-        <main>
+        <article>
           {/* Media Player */}
-          <div className={classes.galleryContainer}>
+          <section className={classes.galleryContainer}>
             <div className={classes.mainImage}>
-              <img src={game.background_image} alt='Game Image' />
+              <img src={game.background_image} alt="Game Image" />
             </div>
             <div className={classes.gallery}>
               <button>
@@ -46,29 +48,29 @@ const GameOverview = () => {
                 <img
                   className={classes.active}
                   src={game.background_image}
-                  alt='Game Image'
+                  alt="Game Image"
                 />
-                <img src={game.background_image} alt='Game Image' />
-                <img src={game.background_image} alt='Game Image' />
+                <img src={game.background_image} alt="Game Image" />
+                <img src={game.background_image} alt="Game Image" />
               </div>
               <button>
                 <FontAwesomeIcon icon={faChevronRight} />
               </button>
             </div>
-          </div>
-          <div className={classes.gameExtras}>
+          </section>
+          <section className={classes.gameExtras}>
             <Collapsable>
               <div className={classes.genreTags}>
                 <span>Genres</span>
                 <div>
                   {game.genres.map((genre, index) => {
-                    const lastItem = game.genres.length - 1 === index
+                    const lastItem = game.genres.length - 1 === index;
                     return (
                       <React.Fragment key={genre.id}>
                         <Link>{genre.name}</Link>
-                        {lastItem ? '' : ', '}
+                        {lastItem ? "" : ", "}
                       </React.Fragment>
-                    )
+                    );
                   })}
                 </div>
               </div>
@@ -78,77 +80,83 @@ const GameOverview = () => {
                 <span>Tags</span>
                 <div>
                   {game.tags.map((tags, index) => {
-                    const lastItem = game.tags.length - 1 === index
+                    const lastItem = game.tags.length - 1 === index;
                     return (
                       <React.Fragment key={tags.id}>
                         <Link>{tags.name}</Link>
-                        {lastItem ? '' : ', '}
+                        {lastItem ? "" : ", "}
                       </React.Fragment>
-                    )
+                    );
                   })}
                 </div>
               </div>
             </Collapsable>
-          </div>
-          <Collapsable size='large'>
+          </section>
+          <Collapsable size="large">
             <div
               className={classes.gameDescription}
               dangerouslySetInnerHTML={{
-                __html: justEnglishDescription(game.description)
+                __html: justEnglishDescription(game.description),
               }}
             />
             <div
               className={classes.gameDescription}
               dangerouslySetInnerHTML={{
-                __html: justEnglishDescription(game.description)
+                __html: justEnglishDescription(game.description),
               }}
             />
           </Collapsable>
-          {/* Achievements section */}
-          <section className={classes.achievementsContainer}>
-            <h5>Available Achievements</h5>
-            <div>
-              {achievements.results.map((achievement) => {
-                return (
-                  <div key={achievement.id} className={classes.achievementCard}>
-                    <img src={achievement.image} alt='Achievemenet image' />
-                    <h4 className='break_lines'>{achievement.name}</h4>
-                    <p className='break_lines break_lines--three'>
-                      {achievement.description}
-                    </p>
-                  </div>
-                )
-              })}
-            </div>
-          </section>
-        </main>
+        </article>
         {/* Game checkout */}
         <aside>
-          <img src={game.background_image} alt='Game Background Image' />
+          <img src={game.background_image} alt="Game Background Image" />
           <span>{randomPriceString()}</span>
-          <Button size='large' bgColor='blue'>
+          <Button size="large" bgColor="blue">
             Buy Now
           </Button>
-          
-          <Button border size='large'>
+
+          <Button border size="large">
             Add to cart
           </Button>
-          <Button border size='large'>
+          <Button border size="large">
             <FontAwesomeIcon icon={faCirclePlus} />
             Add to wishlist
           </Button>
           <div>
-            <span>Publisher</span><span>{game.publishers[0].name}</span>
+            <span>Publisher</span>
+            <span>{game.publishers[0].name}</span>
           </div>
           <div>
-            <span>Release Date</span><span>{game.released}</span>
+            <span>Release Date</span>
+            <span>{game.released}</span>
           </div>
         </aside>
-      </div>
-      <div>{/* Archivements */}</div>
-      <div>{/* Sistem req */}</div>
-    </div>
-  )
+      </section>
+      <section>
+        {/* Achievements section */}
+        <section className={classes.achievementsContainer}>
+          <h5>Available Achievements</h5>
+          <div>
+            {achievements.results.map((achievement) => {
+              return (
+                <div key={achievement.id} className={classes.achievementCard}>
+                  <img src={achievement.image} alt="Achievemenet image" />
+                  <h4 className="break_lines">{achievement.name}</h4>
+                  <p className="break_lines break_lines--three">
+                    {achievement.description}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          <Button size="large" bgColor="gray">
+            See All {achievements.count} Achievements
+          </Button>
+        </section>
+      </section>
+      {/* <Rating rating={game.rating} /> */}
+    </article>
+  );
 }
 
 export default GameOverview
