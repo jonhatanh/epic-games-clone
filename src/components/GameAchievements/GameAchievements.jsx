c3289a23f28c1660082fa78def1dde962c18703f
jonhatanh <jonhatan.higuera@gmail.com>
2024-03-07 11:04:57 -0600

    Finish GameAchievementsComponent

--- src/components/GameAchievements/GameAchievements.jsx
+++ src/components/GameAchievements/GameAchievements.jsx
@@ -7,11 +7,11 @@ import {
   faCaretRight,
   faChevronLeft,
   faChevronRight,
-  faCirclePlus,
+  faCirclePlus
 } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-regular-svg-icons'
 import Button from '../Button/Button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Collapsable from '../Collapsable/Collapsable'
 import Rating from '../Rating/Rating'
 
@@ -23,34 +23,53 @@ function justEnglishDescription (description) {
 const GameAchievements = () => {
   const { game, achievements } = useRouteLoaderData('showGame')
   console.log(game)
+  const [nextPage, setNextPage] = useState(achievements.next)
+  const [allAchievements, setAllAchievements] = useState(achievements.results)
+
+  async function handleClick () {
+    const res = await fetch(nextPage)
+    const newAchievements = await res.json()
+    setNextPage(newAchievements.next)
+    setAllAchievements([...allAchievements, ...newAchievements.results])
+  }
+
   return (
     <article className={classes.container}>
       <section className={classes.achievementsHero}>
-        <img src={game.background_image} alt="Game Background Image" />
-        <div>
-          <p>
-            <span>Available achievements</span>
-            <span>{achievements.count} Achievements</span>
-          </p>
-        </div>
+        <img src={game.background_image} alt='Game Background Image' />
+        <p>
+          <span>Available achievements</span>
+          <span>{achievements.count} Achievements</span>
+        </p>
       </section>
       <main>
-        {achievements.results.map((achievement) => {
-          return (
-            <div key={achievement.id} className={classes.achievementCard}>
-              <img src={achievement.image} alt="Achievemenet image" />
-              <div>
-                <h4 className="break_lines">{achievement.name}</h4>
-                <p className="break_lines break_lines--three">
-                  {achievement.description}
-                </p>
+        <h3>
+          Achievements <span>({achievements.count})</span>
+        </h3>
+        <section className={classes.achievementsContainer}>
+          {allAchievements.map((achievement) => {
+            return (
+              <div key={achievement.id} className={classes.achievementCard}>
+                <img src={achievement.image} alt='Achievemenet image' />
+                <div>
+                  <h4 className='break_lines'>{achievement.name}</h4>
+                  <p className='break_lines break_lines--three'>
+                    {achievement.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            )
+          })}
+        </section>
+
+        {nextPage !== null && (
+          <Button onClick={handleClick} size='large' bgColor='gray'>
+            Show more
+          </Button>
+        )}
       </main>
     </article>
-  );
+  )
 }
 
 export default GameAchievements
